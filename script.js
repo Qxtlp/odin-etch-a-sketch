014f1ba89457fa437fc30d5cc0877cebb6d9859b
Qxtlp <xi.tulop@yandex.com>
2022-03-02 16:12:27 +0700

    add background color on hover feature

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ document.body.onload = addGrid;
 function addGrid() {
     for (let i = 1; i <= 16 * 16; i++) {
         const column = document.createElement("div");
+        column.addEventListener("mouseover", () => {
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            column.style.background = "#" + randomColor;
+        })
         column.className = "item";
         document.getElementById("grid").appendChild(column);
-
     }
 }
