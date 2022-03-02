90e3bd258455cbcac5bb40c54badc02fa7c68b5f
Qxtlp <xi.tulop@yandex.com>
2022-03-02 19:34:21 +0700

    add change grid size feature

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-document.body.onload = addGrid;
-
-function addGrid() {
-    for (let i = 1; i <= 16 * 16; i++) {
+function addGrid(size) {
+    const grid = document.querySelector("#grid");
+    const child = grid.querySelectorAll("div");
+    child.forEach(child => child.remove());
+    grid.style.cssText = `grid-template-columns: repeat(${size}, 1fr) `
+    console.log(size);
+    for (let i = 1; i <= size * size; i++) {
         const column = document.createElement("div");
         column.addEventListener("mouseover", () => {
             let randomColor = Math.floor(Math.random() * 16777215).toString(16);
